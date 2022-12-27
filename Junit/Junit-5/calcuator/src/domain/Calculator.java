f4c1e07a2c04593d75345aedb6351bd3917f0662
Nikolas Gou <72390598+nikolas-gou@users.noreply.github.com>
2022-12-27 02:50:22 +0200

    Update Calculator.java

--- Junit/Junit-5/calcuator/src/domain/Calculator.java
+++ Junit/Junit-5/calcuator/src/domain/Calculator.java
@@ -3,7 +3,7 @@ package domain;
 public class Calculator {
 	public double add(double num1, double num2) {
 		return num1 + num2;
-		}
+	}
 	
 	public double subtract(double num1, double num2) {
 		return num1 - num2;
