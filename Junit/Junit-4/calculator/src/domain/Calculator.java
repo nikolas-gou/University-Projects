fa7f9cb4249a924c148097880692fe455e158e29
Nikolas Gou <72390598+nikolas-gou@users.noreply.github.com>
2022-12-27 02:49:30 +0200

    Update Calculator.java

--- Junit/Junit-4/calculator/src/domain/Calculator.java
+++ Junit/Junit-4/calculator/src/domain/Calculator.java
@@ -3,16 +3,16 @@ package domain;
 public class Calculator {
 	public double add(double num1, double num2) {
 		return num1 + num2;
-		}
+	}
 	
 	public int fib(int n) {
 		 int prev1 = 0, prev2 = 1;
 		 for (int i = 0; i < n; i++) {
-		 int savePrev1 = prev1;
-		 prev1 = prev2;
-		 prev2 = savePrev1 + prev2;
+			 int savePrev1 = prev1;
+			 prev1 = prev2;
+			 prev2 = savePrev1 + prev2;
 		 }
 		 return prev1;
-		} 
+	} 
 
 }
