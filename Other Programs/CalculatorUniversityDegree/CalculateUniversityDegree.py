fee11c5b30629935bfdee0ca4f2c0351dc185c6c
Nikolas Gou <72390598+nikolas-gou@users.noreply.github.com>
2023-02-05 01:20:48 +0200

    Update CalculateUniversityDegree.py

--- Other Programs/CalculatorUniversityDegree/CalculateUniversityDegree.py
+++ Other Programs/CalculatorUniversityDegree/CalculateUniversityDegree.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-df = pd.read_excel('degrees.xlsx')
+df = pd.read_excel('degrees.xlsx');
 
-array_1 = df.iloc[:, 1].values
-array_2 = df.iloc[:, 2].values
+array_1 = df.iloc[:, 1].values;
+array_2 = df.iloc[:, 2].values;
 
 sum = 0;
 total_ects = 0;
@@ -13,11 +13,4 @@ for i in range(0, 39):
         sum += array_1[i] * array_2[i];
         total_ects += array_2[i];
 
-print("Your university degree is: ", sum/total_ects)
-
-for i in range(0, 39):
-    if array_1[i] >= 5:
-        sum += array_1[i] * array_2[i];
-        total_ects += array_2[i];
-
-
+print("Your university degree is: ", sum/total_ects);
